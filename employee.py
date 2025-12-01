10d0303814e9bde511ba681f832c0d58c50784bc
Deepa Jadhav <deepajadhav2001@gmail.com>
2025-12-01 13:09:11 +0530

    Update employee.py

--- employee.py
+++ employee.py
@@ -12,19 +12,10 @@ def employee_details(name, emp_id, department, salary):
 
 # ---- MAIN METHOD USING sys.argv ----
 if __name__ == "__main__":
-    # sys.argv[0] → script name
-    # sys.argv[1] → name
-    # sys.argv[2] → emp_id
-    # sys.argv[3] → department
-    # sys.argv[4] → salary
-
     if len(sys.argv) != 5:
-        print("Usage: python employee.py <name> <emp_id> <department> <salary>")
-        sys.exit(1)
+        name = sys.argv[1]
+        emp_id = sys.argv[2]
+        department = sys.argv[3]
+        salary = sys.argv[4]
 
-    name = sys.argv[1]
-    emp_id = sys.argv[2]
-    department = sys.argv[3]
-    salary = sys.argv[4]
-
-    print(employee_details(name, emp_id, department, salary))
+        print(employee_details(name, emp_id, department, salary))
