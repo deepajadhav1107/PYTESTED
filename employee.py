ce11c9fbe7cf26f19241716950dfdcdb87c54639
Deepa Jadhav <deepajadhav2001@gmail.com>
2025-12-01 13:12:37 +0530

    Update employee.py

--- employee.py
+++ employee.py
@@ -1,21 +1,17 @@
-import sys
-import pytest   # as you requested
+import pytest   
 
 def employee_details(name, emp_id, department, salary):
     result = (
         f"Employee Name: {name}\n"
-        f"Employee ID: {emp_id}\n"
+        f"Employee ID: {name}\n"
         f"Department: {department}\n"
         f"Salary: {salary}"
     )
     return result
 
-# ---- MAIN METHOD USING sys.argv ----
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        name = sys.argv[1]
-        emp_id = sys.argv[2]
-        department = sys.argv[3]
-        salary = sys.argv[4]
-
-        print(employee_details(name, emp_id, department, salary))
+    name = "Alice"
+    emp_id = "E1001"
+    department = "IT"
+    salary = 55000
+    print(employee_details(name, emp_id, department, salary))
